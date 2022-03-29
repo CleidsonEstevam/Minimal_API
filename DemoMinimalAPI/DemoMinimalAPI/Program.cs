7965556243406ede42a30503e2e1e6699933f8ff
Cleidson Estevam Da Silva <cleidson.silva@reval.corp>
2022-03-29 13:40:17 -0300

    UpdateRota

--- DemoMinimalAPI/DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/DemoMinimalAPI/Program.cs
@@ -1,6 +1,7 @@
 using DemoMinimalAPI.Data;
 using DemoMinimalAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,7 +37,7 @@ app.MapGet("/fornecedor", async (
 app.MapGet("/fornecedor/{id}", async (
     Guid id,
     MinimalContextDb context) =>
-        
+
     await context.Fornecedores.FindAsync(id)
           is Fornecedor fornecedor
                 ? Results.Ok(fornecedor)
@@ -45,6 +46,28 @@ app.MapGet("/fornecedor/{id}", async (
     .Produces<Fornecedor>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound)
     .WithName("GetFornecedorPorId")
+    .WithTags("Fornecedor");
+
+
+
+app.MapPost("/fornecedor", async (
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+{
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+    ? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor)
+    : Results.BadRequest("Problema ao salvar registro.");
+    
+})
+    .Produces<Fornecedor>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("PostFornecedorPorId")
     .WithTags("Fornecedor");
 
 app.Run();
